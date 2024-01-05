9bae205f768b43bd4426644cd5d258f0fe4fb1ba
Alikhan Aitbayev <y>
2024-01-05 12:04:43 +0600

    5.c

--- HW2/subarray.py
+++ HW2/subarray.py
@@ -1,8 +1,11 @@
+MINUS_INFINITY = float("-inf")
+
+
 def max_subarray_sum(nums):
     if not nums:
         return None
 
-    max_sum= nums[0]
+    max_sum = nums[0]
     curr_sum = nums[0]
 
     for i in range(1, len(nums)):
@@ -11,14 +14,16 @@ def max_subarray_sum(nums):
 
     return max_sum
 
+
 def max_subarray_sum_2(nums):
     pass
+
 
 def max_subarray_sum_length_range(nums, A, B):
     if not nums or A > B or A <= 0 or B <= 0:
         return None
 
-    max_sum = float('-inf')
+    max_sum = MINUS_INFINITY
     curr_sum = 0
     curr_length = 0
     left = 0
@@ -35,50 +40,45 @@ def max_subarray_sum_length_range(nums, A, B):
         if A <= curr_length <= B:
             max_sum = max(max_sum, curr_sum)
 
-    return max_sum if max_sum != float('-inf') else None
+    return max_sum if max_sum != MINUS_INFINITY else None
 
 
 def max_subarray_sum_with_k_elem(nums, k):
-    if not nums or k > len(nums):
-        return None
+    left = 0
+    counter = {}
+    curr_sum = 0
+    max_sum = MINUS_INFINITY
 
-    l, r = 0, k
-    max_sum = 0
-    unique = set()
-    i = 0
-    while len(unique) < k and i < len(nums):
-        max_sum += nums[i]
-        unique.add(nums[i])
-        i += 1
+    for right in range(len(nums)):
+        num = nums[right]
 
-    curr_sum = max_sum
-    while r < len(nums):
-        print(r, unique, curr_sum, max_sum)
-        while r < len(nums) and nums[r] in unique:
-            curr_sum += nums[r]
-            r+=1
-        if r < len(nums):
-            curr_sum = curr_sum - nums[l] + nums[r]
-            unique.add(nums[r])
-            unique.discard(nums[l])
+        if num in counter:
+            counter[num] += 1
+        else:
+            counter[num] = 1
+
+        curr_sum += num
+
+        while counter[num] > 1 or (right - left + 1) > k:
+            curr_sum -= nums[left]
+            counter[nums[left]] -= 1
+            left += 1
+
+        if right - left + 1 == k:
             max_sum = max(max_sum, curr_sum)
-            r+=1
-            l+=1
-    return max_sum
+
+    return max_sum if max_sum != MINUS_INFINITY else None
 
 
-
-
-arr1 = [-2,1,-3,4,-1,2,1,-5,4]
-arr2 = [5,4,-1,7,8]
+arr1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+arr2 = [5, 4, -1, 7, 8]
 arr = [1, -2, 3, 10, -4, 7, 2, -5]
-arr3 = [1,5,4,2,9,9,9]
-arr4 = [4,4,4]
+arr3 = [1, 5, 4, 2, 9, 9, 9]
+arr4 = [4, 4, 4]
 
 # print(max_subarray_sum(arr2))
-# print(max_subarray_sum_with_k_elem(arr1, 4))
-# max_subarray_sum_with_length(arr1, 1, 1)
 # print(max_subarray_sum_length_range(arr1, 1,1))
-print(max_subarray_sum_with_k_elem(arr4, 3))
+# print(max_subarray_sum_with_k_elem(arr1, 4))
+# print(max_subarray_sum_with_k_elem(arr4, 3))
 
 
