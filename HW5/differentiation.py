263cee724a995bc76badf1c22eee778e8a27327a
Alikhan Aitbayev <y>
2024-01-26 16:48:36 +0600

    done

--- HW5/differentiation.py
+++ HW5/differentiation.py
@@ -1,3 +1,23 @@
+import inspect
+def peel(cls):
+    if inspect.isclass(cls):
+        res = {s for s in dir(cls) if not s.startswith("_")}
+        return res
+
+    
+
+def implements(interface):
+    def wraps(cls):
+        list_of_cls = {m for m in dir(cls) if not m.startswith("_")}
+        list_to_impl = {m for m in dir(interface) if not m.startswith("_")}
+        if list_of_cls >= list_to_impl:
+            return cls
+        diff = " ".join(list_to_impl.difference(list_of_cls))
+        assert False, f"method '{diff}' not implemented"
+    return wraps
+     
+
+
 class Expr:
     def __call__(self, **context):
         pass
@@ -5,25 +25,48 @@ class Expr:
     def d(self, wrt):
         pass
 
+    @property
+    def is_constexpr(self):
+        pass
+
+    @property
+    def simplified(self):
+        pass
+
     def __neg__(self):
         return Product(Const(-1), self)
+    
     def __pos__(self):
-        self
+        return self
+
     def __add__(self, e2):
         return Sum(self, e2)
+    
     def __sub__(self, e2):
         return Sum(self, -e2)
+    
     def __mul__(self, e2):
         return Product(self, e2)
+    
     def __truediv__(self, e2):
         return Fraction(self, e2)
+    
     def __pow__(self, e2):
         return Power(self, e2)
+    
 
 class Const(Expr):
     def __init__(self, val):
         self.val = val
 
+    @property
+    def is_constexpr(self):
+        return True
+
+    @property
+    def simplified(self):
+        return self
+    
     def __str__(self):
         return f"{self.val}"
 
@@ -36,6 +79,14 @@ class Const(Expr):
 class Var(Expr):
     def __init__(self, var):
         self.var = var
+
+    @property
+    def is_constexpr(self):
+        return False
+
+    @property
+    def simplified(self):
+        return self
 
     def __str__(self):
         return f"{self.var}"
@@ -53,18 +104,27 @@ def V(name):
 def C(val):
     return Const(val)
 
-# print(C(42)())
-# print(C(42).d(V("x"))())
-# print(V("x")(x=42))
-# print(V("x").d(V("x"))())
-# print(V("x").d(V("y"))())
 
 x = V("x")
 y = V("y") 
+
 class BinOp(Expr):
     def __init__(self, expr1, expr2):
         self.expr1, self.expr2 = expr1, expr2
     
+    @property
+    def is_constexpr(self):
+        return self.expr1.is_constexpr and self.expr2.is_constexpr
+
+    @property
+    def simplified(self):
+        expr1_simplified = self.expr1.simplified
+        expr2_simplified = self.expr2.simplified
+
+        if expr1_simplified.is_constexpr and expr2_simplified.is_constexpr:
+            return Const(self.__class__(expr1_simplified, expr2_simplified)())
+        return self.__class__(expr1_simplified, expr2_simplified)
+
     def operator(self):
         pass
 
@@ -72,8 +132,6 @@ class BinOp(Expr):
         return f"({self.operator()} {self.expr1} {self.expr2})"
 
 class Sum(BinOp):
-    # def __str__(self):
-    #     return f"(+ {self.expr1} {self.expr2})"
     def operator(self):
         return "+"
 
@@ -83,9 +141,6 @@ class Sum(BinOp):
     def d(self, wrt):
         return Sum(self.expr1.d(wrt), self.expr2.d(wrt))
 
-# print(Sum(x, y)(x = 1, y = 3))
-# print(Sum(x, y).d(x)(x = 2, y=3))
-# print(Sum(x, y)(x=1,y=4))
 
 class Product(BinOp):
     def operator(self):
@@ -97,10 +152,6 @@ class Product(BinOp):
     def d(self, wrt):
         return Sum(Product(self.expr1.d(wrt), self.expr2), Product(self.expr1, self.expr2.d(wrt)))
 
-# print(Product(x, y)(x=2, y=3))
-# print(Product(x, y).d(x)(x=2, y=3))
-
-
 class Fraction(BinOp):
     def operator(self):
         return "/"
@@ -110,18 +161,6 @@ class Fraction(BinOp):
     
     def d(self, wrt):
         return Fraction(Sum(Product(self.expr2, self.expr1.d(wrt)), Product(Product(self.expr1, self.expr2.d(wrt)), Const(-1))), Product(self.expr2, self.expr2))
-
-# print(Fraction(x, y)(x = 4, y = 5))
-# print(Fraction(x, y).d(x)(x = 4, y = 5))
-
-# print(Sum(x, Product(x, x)).d(x)(x=42)) 
-
-# print(Product(x, Sum(x, C(2)))(x=42))
-
-# print(Fraction(Product(x, y), Sum(C(42), x)).d(x)(x=42, y=24))  # 0.14285714285714285
-
-# print(Fraction(Product(x, y), Sum(C(42), x)).d(y)(x=42, y=24))  # 0.5
-
 
 
 class Power(BinOp):
@@ -135,17 +174,6 @@ class Power(BinOp):
     def d(self, wrt):
         return Product(Product(Power(self.expr1, Sum(self.expr2, Const(-1))), self.expr2), self.expr1.d(wrt))
 
-
-# print(Power(Fraction(V("x"), C(4)), C(2))(x=42))
-# print(Power(Fraction(V("x"), C(4)), C(2)).d(V("x"))(x=42))
-# print(Power(Fraction(V("x"), C(4)), C(-9))(x=42))
-    
-
-# print(Sum(x, Product(x, x)).d(x))
-# print(Product(x, Sum(x, C(2))))
-# print(Power(Fraction(x, C(4)), C(2)))
-
-# print((C(1) - V("x")) ** C(3) + V("x"))
     
 def newton_raphson(f, x0, threshold):
     print(threshold)
@@ -159,7 +187,3 @@ def newton_raphson(f, x0, threshold):
         x = x_next
 
 
-expr = (V("x") + C(-1)) ** C(3) + V("x")
-zero = newton_raphson(expr, 0.5, threshold=1e-4)
-print(expr)
-print(zero, expr(x=zero))
