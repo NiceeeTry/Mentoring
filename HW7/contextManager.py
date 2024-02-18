144feede77b6fc396d0dae57cac302e9f222f4fa
Alikhan Aitbayev <y>
2024-02-18 19:02:19 +0600

    1.b 1.c 1.d

--- HW7/contextManager.py
+++ HW7/contextManager.py
@@ -19,3 +19,61 @@ with assert_raises(ValueError):
 #     pass
 # with assert_raises(ValueError):
 #     raise TypeError
+    
+
+class closing:
+    def __init__(self, obj):
+        self.obj = obj
+    
+    def __enter__(self):
+        return self.obj
+    
+    def __exit__(self, *err):
+        self.obj.close()
+
+with closing(open("example.txt")) as handle:
+    copy = handle
+
+# print(copy.closed)
+    
+
+from traceback import print_exception
+
+class log_exceptions:
+
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, type, value, traceback):
+        if type is not None:
+            print_exception(type, value, traceback)
+            return True
+        return False
+
+def f():
+    with log_exceptions():
+        {}["foobar"]
+    return 42
+
+# print(f())
+from functools import wraps
+
+def with_context(context):
+    def wrapper(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            with context:
+                return func(*args, **kwargs)
+        return inner
+    return wrapper
+
+# from contextlib import redirect_stdout
+# import io
+# handle = io.StringIO()
+
+# @with_context(redirect_stdout(handle))
+# def f():
+#     print("Hello world!")
+
+# f()
+# print(handle.getvalue())
