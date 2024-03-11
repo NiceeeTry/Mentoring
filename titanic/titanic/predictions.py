5bd799babf346b056722e6db272e0efaec267318
Alikhan Aitbayev <y>
2024-03-11 19:56:10 +0500

    pre-commit fixing

--- titanic/titanic/predictions.py
+++ titanic/titanic/predictions.py
@@ -16,13 +16,16 @@ RESULT_DATA_PATH = "../data/predictions.csv"
 
 
 class Titanic:
-
     def __init__(self) -> None:
         self.titanic_data = pd.read_csv(TRAIN_DATA_PATH)
         self.titanic_test_data = pd.read_csv(TEST_DATA_PATH)
-        self.pipline = Pipeline([("ageimputer", AgeImputer()), 
-                    ("featureencoder", FeatureEncoder()), 
-                    ("featuredropper", FeatureDropper())])
+        self.pipline = Pipeline(
+            [
+                ("ageimputer", AgeImputer()),
+                ("featureencoder", FeatureEncoder()),
+                ("featuredropper", FeatureDropper()),
+            ]
+        )
 
     def run(self) -> None:
         strat_train_set, strat_test_set = self.data_splitting(self.titanic_data)
@@ -40,40 +43,45 @@ class Titanic:
         predictions = prod_final_clf.predict(X_data_final_test)
         self.create_predictions(predictions)
 
-
     def data_splitting(self, data: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
         split = StratifiedShuffleSplit(n_splits=1, test_size=0.2)
         for train_indices, test_indices in split.split(data, data[["Survived", "Pclass", "Sex"]]):
             strat_train_set = data.loc[train_indices]
             strat_test_set = data.loc[test_indices]
         return strat_train_set, strat_test_set
-    
 
-    def data_preprocessing(self, strat_train_set: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+    def data_preprocessing(
+        self, strat_train_set: pd.DataFrame
+    ) -> tuple[pd.DataFrame, pd.DataFrame]:
         strat_train_set = self.pipline.fit_transform(strat_train_set)
         X_data, Y_data = self.training_data_preparation(strat_train_set)
-        return X_data, Y_data 
-    
+        return X_data, Y_data
 
-    def training_data_preparation(self, training_set: pd.DataFrame) -> tuple[np.ndarray, np.ndarray]:
+    def training_data_preparation(
+        self, training_set: pd.DataFrame
+    ) -> tuple[np.ndarray, np.ndarray]:
         X = training_set.drop(["Survived"], axis=1)
         Y = training_set["Survived"]
         scaler = StandardScaler()
         X_data = scaler.fit_transform(X)
         Y_data = Y.to_numpy()
         return X_data, Y_data
-    
 
     def trainig(self, X_data: np.ndarray, Y_data: np.ndarray) -> RandomForestClassifier:
         clf = RandomForestClassifier()
         param_grid = [
-            {"n_estimators": [10, 100, 200, 500], "max_depth": [None, 5, 10], "min_samples_split": [2,3,4]}
+            {
+                "n_estimators": [10, 100, 200, 500],
+                "max_depth": [None, 5, 10],
+                "min_samples_split": [2, 3, 4],
+            }
         ]
-        grid_search = GridSearchCV(clf, param_grid, cv=3, scoring="accuracy", return_train_score=True)
+        grid_search = GridSearchCV(
+            clf, param_grid, cv=3, scoring="accuracy", return_train_score=True
+        )
         grid_search.fit(X_data, Y_data)
         final_clf = grid_search.best_estimator_
         return final_clf
-    
 
     def prediction_data_preprocessing(self) -> np.ndarray:
         X_final_test = self.pipline.fit_transform(self.titanic_test_data)
@@ -81,32 +89,27 @@ class Titanic:
         scaler = StandardScaler()
         X_data_final_test = scaler.fit_transform(X_final_test)
         return X_data_final_test
-    
 
     def create_predictions(self, predictions: np.ndarray) -> None:
         final_df = pd.DataFrame(self.titanic_test_data["PassengerId"])
         final_df["Survived"] = predictions
         final_df.to_csv(RESULT_DATA_PATH, index=False)
         # print(final_df)
-    
+
 
 class AgeImputer(BaseEstimator, TransformerMixin):
-
     def fit(self, X: pd.DataFrame, y=None) -> BaseEstimator:
         return self
-    
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
         imputer = SimpleImputer(strategy="mean")
         X["Age"] = imputer.fit_transform(X[["Age"]])
         return X
- 
+
 
 class FeatureEncoder(BaseEstimator, TransformerMixin):
-
     def fit(self, X: pd.DataFrame, y=None) -> BaseEstimator:
         return self
-    
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
         encoder = OneHotEncoder()
@@ -115,18 +118,17 @@ class FeatureEncoder(BaseEstimator, TransformerMixin):
         column_names = ["C", "S", "Q", "N"]
         for i in range(len(matrix.T)):
             X[column_names[i]] = matrix.T[i]
-        
+
         matrix = encoder.fit_transform(X[["Sex"]]).toarray()
         column_names = ["Female", "Male"]
         for i in range(len(matrix.T)):
             X[column_names[i]] = matrix.T[i]
         return X
-    
+
 
 class FeatureDropper(BaseEstimator, TransformerMixin):
     def fit(self, X: pd.DataFrame, y=None) -> BaseEstimator:
         return self
-    
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
         return X.drop(["Embarked", "Name", "Ticket", "Cabin", "Sex", "N"], axis=1, errors="ignore")
@@ -135,3 +137,4 @@ class FeatureDropper(BaseEstimator, TransformerMixin):
 if __name__ == "__main__":
     titantic = Titanic()
     titantic.run()
+
